a4c954a84987033917d3170a42c59f67f2f156dd
hvhai <hunterx6323@gmail.com>
2023-06-19 10:50:05 +0700

    chore: first commit

---
+++ src/main/java/com/codehunter/springtransactionbestpractice/SpringTransactionBestpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codehunter.springtransactionbestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionBestpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransactionBestpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codehunter/springtransactionbestpractice/SpringTransactionBestpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehunter.springtransactionbestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionBestpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
